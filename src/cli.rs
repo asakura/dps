718ac8efaea3acf7de996c74a4b5c020d8fd31fb
Mikalai Seva <mikalai.seva@gmail.com>
2026-05-11 10:07:31 +0200

    fix(cli): use git describe as version when available, omit stray dash

--- src/cli.rs
+++ src/cli.rs
@@ -16,24 +16,18 @@ pub struct Cli {
     pub frame_rate: f64,
 }
 
-const VERSION_MESSAGE: &str = concat!(
-    env!("CARGO_PKG_VERSION"),
-    "-",
-    env!("VERGEN_GIT_DESCRIBE"),
-    " (",
-    env!("VERGEN_BUILD_DATE"),
-    ")"
-);
-
 pub fn version() -> &'static str {
     static VERSION: OnceLock<String> = OnceLock::new();
     VERSION.get_or_init(|| {
+        let git_describe = env!("VERGEN_GIT_DESCRIBE");
+        let version = if git_describe.is_empty() { env!("CARGO_PKG_VERSION") } else { git_describe };
+        let version_message = format!("{} ({})", version, env!("VERGEN_BUILD_DATE"));
         let author = clap::crate_authors!();
         let config_dir_path = get_config_dir().display().to_string();
         let data_dir_path = get_data_dir().display().to_string();
         format!(
             "\
-{VERSION_MESSAGE}
+{version_message}
 
 Authors: {author}
 
