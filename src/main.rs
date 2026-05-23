47d923c15d480ec5d303caa24b28606188efa49d
Mikalai Seva <mikalai.seva@gmail.com>
2026-05-23 19:17:25 +0200

    fix(main): make App::new error conversion to color_eyre::Report explicit

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,26 @@ use dps::logging;
 #[tokio::main]
 async fn main() -> Result<()> {
     errors::init()?;
+
     let args = Cli::parse();
+
     args.validate().unwrap_or_else(|e| e.exit());
+
     let effective_data_dir = args
         .data_dir
         .as_deref()
         .map_or_else(get_data_dir, Path::to_path_buf);
+
     logging::init(&effective_data_dir)?;
+
     let mut app = App::new(
         args.tick_rate,
         args.frame_rate,
         args.config_dir.as_deref(),
         args.data_dir.as_deref(),
-    )?;
+    )
+    .map_err(color_eyre::Report::from)?;
     app.run().await?;
+
     Ok(())
 }
